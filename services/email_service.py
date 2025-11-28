5e7bc5f9a6df3e2a8904975e00ebc7cd3da5305b
Otabek <burhonovotabek5@gmail.com>
2025-11-28 09:41:49 +0500

    completed email sender

--- services/email_service.py
+++ services/email_service.py
@@ -1,2 +1,28 @@
+import os
+import dotenv
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+dotenv.load_dotenv()
+def send_email(to_email, subject, message):
+    sender_email = os.getenv("SENDER_EMAIL")
+    sender_password = os.getenv("SENDER_PASSWORD")
+
+    msg = MIMEMultipart()
+    msg["From"] = sender_email
+    msg["To"] = to_email
+    msg["Subject"] = subject
+
+    msg.attach(MIMEText(message, "plain"))
+
+    try:
+        server = smtplib.SMTP("smtp.gmail.com",587)
+        server.starttls()
+        server.login(sender_email,sender_password)
+
+        server.sendmail(sender_email,to_email,msg.as_string())
+        server.quit()
+
+    except Exception as e:
+        print(f"Error: {e}")
+    
